978117133aefb29c0e93b0cadbf63985ded590c7
GugzNutz <77459593+GugzNutz@users.noreply.github.com>
2022-11-14 11:40:59 +0000

    Add files via upload

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,35 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gfilipe- <gfilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/14 11:34:28 by gfilipe-          #+#    #+#             */
-/*   Updated: 2022/11/14 11:40:14 by gfilipe-         ###   ########.fr       */
+/*   Created: 2022/11/14 11:39:37 by gfilipe-          #+#    #+#             */
+/*   Updated: 2022/11/14 11:40:23 by gfilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	size_t	i;
-	size_t	len;
-	char	*ptr;
 
-	i = 0;
-	len = ft_strlen(s);
-	if (!f || !s)
-		return (0);
-	ptr = (char *)malloc(sizeof(char) * (len + 1));
-	if (ptr == 0)
-		return (0);
-	while (i < len)
-	{
-		ptr[i] = f(i, s[i]);
-		i++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gfilipe- <gfilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 14:16:14 by gfilipe-          #+#    #+#             */
-/*   Updated: 2022/11/14 09:23:42 by gfilipe-         ###   ########.fr       */
+/*   Updated: 2022/11/14 11:40:22 by gfilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,12 +48,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
-/*char	*ft_itoa(int n);
+char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned
 int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int,
 char*));
-void	ft_putchar_fd(char c, int fd);
+/*void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
