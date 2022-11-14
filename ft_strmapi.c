ec38027fad1533bf028f0574e8b85c1e02289356
GugzNutz <77459593+GugzNutz@users.noreply.github.com>
2022-11-14 11:38:53 +0000

    Add files via upload

---
+++ ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfilipe- <gfilipe-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 11:34:28 by gfilipe-          #+#    #+#             */
+/*   Updated: 2022/11/14 11:38:39 by gfilipe-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	i;
+	size_t	len;
+	char	*ptr;
+
+	i = 0;
+	len = ft_strlen(s);
+	if (!f || !s)
+		return (0);
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	if (ptr == 0)
+		return (0);
+	while (i < len)
+	{
+		ptr[i] = f(i, s[i]);
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
