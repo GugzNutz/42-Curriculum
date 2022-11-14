d02dcea75a6e6cf2b6230cb4175a7071c9e82b8c
GugzNutz <77459593+GugzNutz@users.noreply.github.com>
2022-11-14 15:04:12 +0000

    Add files via upload

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: gfilipe- <gfilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 13:25:29 by gfilipe-          #+#    #+#             */
-/*   Updated: 2022/11/14 13:57:50 by gfilipe-         ###   ########.fr       */
+/*   Updated: 2022/11/14 14:59:41 by gfilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	write(fd, ft_itoa(n), 1);
+	 char	c;
+	 long int	i;
 
+	 i = n;
+	if (i < 0)
+	{
+		write(fd, "-", 1);
+		i *= -1;
+	}
+	if (i >= 10)
+	{
+		ft_putnbr_fd(i / 10, fd);
+		ft_putnbr_fd(i % 10, fd);
+	}
+	else
+	{
+		c = i + '0';
+		write(fd, &c, 1);
+	}
 }
