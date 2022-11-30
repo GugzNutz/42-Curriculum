120e5033cb81c077c552c8e2be34fe23a90fbb72
GugzNutz <77459593+GugzNutz@users.noreply.github.com>
2022-11-30 17:36:59 +0000

    Add files via upload

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gfilipe- <gfilipe-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:39:13 by gfilipe-          #+#    #+#             */
-/*   Updated: 2022/11/28 18:04:24 by gfilipe-         ###   ########.fr       */
+/*   Updated: 2022/11/30 17:34:48 by gfilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,51 @@
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*temp;
 	t_list	*sub;
+	t_list	*new;
 
-	if (!lst || !del || !f)
-		return ;
-	temp = (*f)(lst->content)
-	return (*lst);
+	/*if (!lst || !del || !f)
+		return ;*/
+	if (lst == NULL)
+		return (NULL);
+	new = 0;
+	while (lst != NULL)
+	{
+		sub = ft_lstnew(f(lst->content));
+		if (sub == NULL)
+		{
+			ft_lstclear(&sub, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, sub);
+		lst = lst->next;
+	}
+	return (new);
 }
+
+	/*t_list	*first;
+	t_list	*new;
+
+	if (!f || !del)
+		return (NULL);
+	first = NULL;
+	while (lst)
+	{
+		if (!(new = ft_lstnew((*f)(lst->content))))
+		{
+			while (first)
+			{
+				new = first->next;
+				(*del)(first->content);
+				free(first);
+				first = new;
+			}
+			lst = NULL;
+			return (NULL);
+		}
+		ft_lstadd_back(&first, new);
+		lst = lst->next;
+	}
+	return (first);
+}
+*/
